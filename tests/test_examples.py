b77d8062137abfb2ddfcb0c30d9085b19eed17ac
Marcin Konowalczyk <marcin.konowalczyk@chem.ox.ac.uk>
2020-08-24 11:02:09 +0100

    Updated example tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -4,7 +4,8 @@ from os.path import exists, splitext
 # TODO change to subprocess.run
 import subprocess
 from functools import partial
-shell = partial(subprocess.call,shell=True)
+shell = partial(subprocess.run,shell=True,capture_output=True)
+# shell = partial(subprocess.call,shell=True)
 
 pyra_path = os.getcwd() + '/Pyramid-Scheme/pyra.rb'
 pyra_exists = exists(pyra_path)
@@ -39,27 +40,45 @@ class MetaTest:
         pyra_filename = path + '.pyra'
         if exists(pyra_filename): os.remove(pyra_filename)
 
-        command = f'python psll.py {self.filename} -o -f'
-        if shell(command) != 0:
-            self.fail('Compilation unsuccessful!')
-        self.assertTrue(
-            exists(pyra_filename),
-            '.pyra file not generated')
+        com = f'python psll.py {self.filename} -o -f'
+        commands = (com, com + ' -go') #, com + ' -so')
+        for com in commands:
+            with self.subTest(command=com):
+                s = shell(com)
+                if s.returncode != 0:
+                    self.fail('Compilation unsuccessful!')
+                else:
+                    # out = s.stdout.decode("utf-8")
+                    # out = (' > ' + l for l in out.split('\n') if l)
+                    # print('\nPsll compiler output:')
+                    # print('\n'.join(out))
+                    pass
+                self.assertTrue(exists(pyra_filename),'.pyra file not generated')
     
     @unittest.skipUnless(ruby_exists,'No ruby found')
     @unittest.skipUnless(pyra_exists,'No pyra.rb found')
     def test_runs(self):
         path, ext = splitext(self.filename)
         pyra_filename = path + '.pyra'
-        if exists(pyra_filename):
-            shell(f'python psll.py {self.filename} -o -f')
         
         # TODO What if the script takes a command line input? (Add timeout?)
         # TODO Capture stdout better
-        command = f'{ruby_path} {pyra_path} {pyra_filename}'
-        if shell(command) != 0:
-            self.fail('Pyramid Scheme code does not run!')
 
+        commands = f'python psll.py {self.filename} -o -f'
+        commands = [commands,commands+' -go']
+        for psll_com in commands:
+            rb_com = f'{ruby_path} {pyra_path} {pyra_filename}'
+            with self.subTest(command=psll_com):
+                shell(psll_com) # Recompile
+                s = shell(rb_com)
+                if s.returncode != 0:
+                    self.fail('Pyramid Scheme code does not run!')
+                else:
+                    # out = s.stdout.decode("utf-8")
+                    # out = (' > ' + l for l in out.split('\n') if l)
+                    # print('\nPyramid Scheme output:')
+                    # print('\n'.join(out))
+                    pass
 @skipUnlessExampleExists(examples + 'nargin_counter.psll')
 class TestNarginCounter(unittest.TestCase,MetaTest):
     pass
