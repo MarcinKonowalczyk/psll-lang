f32c897ef422ac1bfd4b388d1908ca1713056ff0
Marcin <marcin.konow@lczyk.xyz>
2020-10-13 17:56:13 +0100

    Early break in coverage

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 declare -a FILES_TO_TEST=("psll" "ascii_trees")
 
 rm -f .coverage # Clear any previous .coverage report files
@@ -9,7 +14,14 @@ do
     echo ""
     echo "Testing $NAME.py"
     coverage run -a --include="$NAME.py" -m unittest discover -p "test_$NAME.py" tests -v
+    
+    # Exit if unittests of this file failed
+    if [ "$?" != "0" ]; then
+        echo -e "${RED}Something is wrong${NC}"
+        exit 1
+    fi
 done
+
 echo ""
 coverage report
 
