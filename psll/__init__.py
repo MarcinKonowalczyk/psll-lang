bb07ed2c96ba73f3c7081bd817296770df0c42f2
Marcin <marcin.konow@lczyk.xyz>
2025-04-03 17:45:17 +0100

    0.1.6

--- psll/__init__.py
+++ psll/__init__.py
@@ -2,7 +2,7 @@
 Macro-driven metalanguage which compiles to [Pyramid Scheme](https://github.com/ConorOBrien-Foxx/Pyramid-Scheme).
 """
 
-__version__ = "0.1.6.dev0"
+__version__ = "0.1.6"
 
 
 class PsllSyntaxError(SyntaxError):
